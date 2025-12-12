569318ac2350d3bdfa51ffd9e628b631f1c0dd8c
udruufa <fedoseefa@gmail.com>
2025-12-12 09:35:17 +0300

    commit

--- cd/js/gallery.js
+++ cd/js/gallery.js
@@ -6,14 +6,12 @@ $(document).ready(function() {
         const photos = galleryHeader.next('.photos'); 
         const scrollAmount = 230;
 
-        // Левая стрелка
         galleryHeader.find('img.scroll').first().on('click', function() {
             photos.animate({
                 scrollLeft: photos.scrollLeft() - scrollAmount
             }, 300);
         });
 
-        // Правая стрелка
         galleryHeader.find('img.scroll').last().on('click', function() {
             photos.animate({
                 scrollLeft: photos.scrollLeft() + scrollAmount
